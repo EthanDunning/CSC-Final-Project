4b3138af45ad1a78ff67b52fe5c8bef12eea32de
Zach Nalepa <zwn001@latech.edu>
2021-04-18 15:46:05 -0500

    basic setup for the sonic sensor

--- Game.py
+++ Game.py
@@ -55,12 +55,12 @@ class Game:
 
 
 # pins
-leds = []
-switches = []
-wires = []
-sonic_sensor = []
-test_led = []
-speaker = []
+# leds = []
+# switches = []
+# wires = []
+# sonic_sensor = []
+# test_led = []
+# speaker = []
 
 # setting up the GPIO
 GPIO.setmode(GPIO.BCM)
@@ -92,3 +92,50 @@ all_on()
 sleep(0.5)
 all_off()
 sleep(0.5)
+
+# ultrasonic sensor
+GPIO_TRIGGER = []
+GPIO_ECHO = <>
+ 
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+ 
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+ 
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = distance()
+            print ("Measured Distance = %.1f cm" % dist)
+            time.sleep(1)
+ 
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
