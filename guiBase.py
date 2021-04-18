e562740c95efe81208607e83797fe2992d11225f
Ethan <ethandunning2002@gmail.com>
2021-04-17 20:10:19 -0500

    Added Things

--- guiBase.py
+++ guiBase.py
@@ -13,15 +13,26 @@ class MainGUI(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent, bg="white")
         parent.attributes("-fullscreen", False)
-        self.mins= 5
+        self.mins=5
         self.secs=0
-        self.hp = 3
-        self.maxhp = 3
+        self.hp=3
+        self.maxhp=3
         self.loc = "Home"
-                
-        self.setupGUI()
+        self.start_screen()
 
+    def start_screen(self):
+
+        button = Button(self, bg="red", text="Push to Start", font=("TexGyreAdventor", 25), borderwidth=10, highlightthickness=0, activebackground="blue", command=lambda: self.setupGUI())
+        button.grid(row=0, column=0, sticky=N+S+E+W, padx=5, pady=5)
+
+        for row in range(1):
+            Grid.rowconfigure(self, row, weight=1)
+            for col in range(1):
+                Grid.columnconfigure(self, col, weight=1)
+
+        self.pack(fill=BOTH, expand=1)
         
+
     def setupGUI(self):
         self.mins= 5
         self.secs=0
@@ -76,48 +87,36 @@ class MainGUI(Frame):
     def health(self):
         
         health = Label(self, text=f"{self.hp}/{self.maxhp}", bg="white", font=("TexGyreAdventor", 45))
-        health.grid(row=0, column=1, sticky=N+S+E+W, padx = 5, pady=5)
+        health.grid(row=0, column=1, sticky=N+S+E+W, padx=5, pady=5)
         
     def location(self):
 
         location = Label(self, text=f"{self.loc}", bg="white", font=("TexGyreAdventor", 45))
-        location.grid(row=0, column=2, sticky=N+S+E+W, padx = 5, pady=5)
+        location.grid(row=0, column=2, sticky=N+S+E+W, padx=5, pady=5)
 
     def Button1(self):
-        img = PhotoImage(file="sub.gif")
         button = Button(self, bg="red", text="Module 1", font=("TexGyreAdventor", 25), borderwidth=10, highlightthickness=0, activebackground="blue", command=lambda: print("pushed"))
-        button.image = img
-        button.grid(row=1, column=0, sticky=N+S+E+W, padx = 5, pady=5)
+        button.grid(row=1, column=0, sticky=N+S+E+W, padx=5, pady=5)
     
     def Button2(self):
-        img = PhotoImage(file="sub.gif")
         button = Button(self, bg="red", text="Module 2", font=("TexGyreAdventor", 25), borderwidth=10, highlightthickness=0, activebackground="blue", command=lambda: print("pushed"))
-        button.image = img
-        button.grid(row=1, column=1, sticky=N+S+E+W, padx = 5, pady=5)
+        button.grid(row=1, column=1, sticky=N+S+E+W, padx=5, pady=5)
     
     def Button3(self):
-        img = PhotoImage(file="sub.gif")
         button = Button(self, bg="red", text="Module 3", font=("TexGyreAdventor", 25), borderwidth=10, highlightthickness=0, activebackground="blue", command=lambda: print("pushed"))
-        button.image = img
-        button.grid(row=1, column=2, sticky=N+S+E+W, padx = 5, pady=5)
+        button.grid(row=1, column=2, sticky=N+S+E+W, padx=5, pady=5)
     
     def Button4(self):
-        img = PhotoImage(file="sub.gif")
         button = Button(self, bg="red", text="Module 4", font=("TexGyreAdventor", 25), borderwidth=10, highlightthickness=0, activebackground="blue", command=lambda: print("pushed"))
-        button.image = img
-        button.grid(row=2, column=0, sticky=N+S+E+W, padx = 5, pady=5)
+        button.grid(row=2, column=0, sticky=N+S+E+W, padx=5, pady=5)
     
     def Button5(self):
-        img = PhotoImage(file="sub.gif")
         button = Button(self, bg="red", text="Module 5", font=("TexGyreAdventor", 25), borderwidth=10, highlightthickness=0, activebackground="blue", command=lambda: print("pushed"))
-        button.image = img
-        button.grid(row=2, column=1, sticky=N+S+E+W, padx = 5, pady=5)
+        button.grid(row=2, column=1, sticky=N+S+E+W, padx=5, pady=5)
     
     def Button6(self):
-        img = PhotoImage(file="sub.gif")
         button = Button(self, bg="red", text="Module 6", font=("TexGyreAdventor", 25), borderwidth=10, highlightthickness=0, activebackground="blue", command=lambda: print("pushed"))
-        button.image = img
-        button.grid(row=2, column=2, sticky=N+S+E+W, padx = 5, pady=5)
+        button.grid(row=2, column=2, sticky=N+S+E+W, padx=5, pady=5)
     
     
 
