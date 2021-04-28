83fd4341f2f5dff79b561c67b05ffd27eb66f852
EthanDunning <ethandunning2002@gmail.com>
2021-04-28 18:17:12 -0500

    Testing

--- Game.py
+++ Game.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO;
-import time;
+from time import *
 #import guiBase as GUI;
 
 # set gpio mode
@@ -61,7 +61,7 @@ class Game:
 
     # other methods 
 
-# pins
+# # pins
 # leds = []
 # switches = []
 # wires = []
@@ -69,80 +69,80 @@ class Game:
 # test_led = []
 # speaker = []
 
-# setting up the GPIO
-GPIO.setmode(GPIO.BCM)
-# I/O
-GPIO.setup(leds, GPIO.OUT)
-GPIO.setup(test_led, GPIO.OUT)
-GPIO.setup(switches, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(wires, GPIO.IN)
+# # setting up the GPIO
+# GPIO.setmode(GPIO.BCM)
+# # I/O
+# GPIO.setup(leds, GPIO.OUT)
+# GPIO.setup(test_led, GPIO.OUT)
+# GPIO.setup(switches, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# GPIO.setup(wires, GPIO.IN)
 
-try:
-	# testing the leds
-	def all_on():
-		for i in leds:
-			GPIO.output(leds, True)
+# try:
+# 	# testing the leds
+# 	def all_on():
+# 		for i in leds:
+# 			GPIO.output(leds, True)
 
-	def all_off():
-		for i in leds:
-			GPIO.output(leds, False)
+# 	def all_off():
+# 		for i in leds:
+# 			GPIO.output(leds, False)
 
-	def switch_test():
-		for i in switches:
-			if i == True:
-				GPIO.output(test_led, True)
+# 	def switch_test():
+# 		for i in switches:
+# 			if i == True:
+# 				GPIO.output(test_led, True)
 
-except KeyboardInterrupt:
-	GPIO.cleanup()
+# except KeyboardInterrupt:
+# 	GPIO.cleanup()
 
-all_on()
-sleep(0.5)
-all_off()
-sleep(0.5)
+# all_on()
+# sleep(0.5)
+# all_off()
+# sleep(0.5)
 
 # ultrasonic sensor
-GPIO_TRIGGER = []
-GPIO_ECHO = <>
+# GPIO_TRIGGER = []
+# GPIO_ECHO = None
  
-#set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+# #set GPIO direction (IN / OUT)
+# GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+# GPIO.setup(GPIO_ECHO, GPIO.IN)
  
-def distance():
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
+# def distance():
+#     # set Trigger to HIGH
+#     GPIO.output(GPIO_TRIGGER, True)
  
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+#     # set Trigger after 0.01ms to LOW
+#     time.sleep(0.00001)
+#     GPIO.output(GPIO_TRIGGER, False)
  
-    StartTime = time.time()
-    StopTime = time.time()
+#     StartTime = time.time()
+#     StopTime = time.time()
  
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
+#     # save StartTime
+#     while GPIO.input(GPIO_ECHO) == 0:
+#         StartTime = time.time()
  
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
+#     # save time of arrival
+#     while GPIO.input(GPIO_ECHO) == 1:
+#         StopTime = time.time()
  
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
+#     # time difference between start and arrival
+#     TimeElapsed = StopTime - StartTime
+#     # multiply with the sonic speed (34300 cm/s)
+#     # and divide by 2, because there and back
+#     distance = (TimeElapsed * 34300) / 2
  
-    return distance
+#     return distance
  
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
+# if __name__ == '__main__':
+#     try:
+#         while True:
+#             dist = distance()
+#             print ("Measured Distance = %.1f cm" % dist)
+#             sleep(1)
  
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+#         # Reset by pressing CTRL + C
+#     except KeyboardInterrupt:
+#         print("Measurement stopped by User")
+#         GPIO.cleanup()
--- guiBase.py
+++ guiBase.py
@@ -4,6 +4,7 @@ from time import *
 from datetime import *
 from random import *
 from math import *
+import Keypad
 
 # the main GUI
 
@@ -20,8 +21,8 @@ class MainGUI(Frame):
     def reset(self):
         self.Alive = True
         self.maxstrikes = 2
-        self.startmins = 0
-        self.startsecs = 15
+        self.startmins = 60
+        self.startsecs = 0
         self.timer_pause = False
         self.mins = self.startmins
         self.secs = self.startsecs
@@ -414,138 +415,133 @@ class MainGUI(Frame):
 
         self.pack(fill=BOTH, expand=True)
 
-    def Module_Keypad(self, started):
-        self.clearFrame()
+    # def Module_Keypad(self, started):
+        # self.clearFrame()
 
-        self.rows = 6
-        self.cols = 6
-        self.loc = "Keypad"
+        # self.rows = 6
+        # self.cols = 6
+        # self.loc = "Keypad"
 
-        self.pause_button(0, 0, 3)
-        self.back_button(0, 3, 3)
-        self.countdown(self.mins, self.secs, 1, 0, 2)
-        self.counter = self.after(1000, self.update_timer, 1, 0, 2)
-        self.location(1, 2, 2)
-        self.health(1, 4, 2)
+        # self.pause_button(0, 0, 3)
+        # self.back_button(0, 3, 3)
+        # self.countdown(self.mins, self.secs, 1, 0, 2)
+        # self.counter = self.after(1000, self.update_timer, 1, 0, 2)
+        # self.location(1, 2, 2)
+        # self.health(1, 4, 2)
 
-        symbols = {1:"a", 2:"n", 3:"R", 4:"D", 5:".", 6:"C", 7:"b", 8:"e", 9:"c", 10:"d", 11:"S", 12:"l", 13:"N", 14:"f", 15:"-", 16:"A", 17:"o", 18:"J", 19:"M", 20:"E", 21:",", 22:"F", 23:"g", 24:"m", 25:"T", 26:"L", 27:"/"}
-        column1 = [1,2,3,13,5,6,7]
-        column2 = [8,1,7,22,10,6,11]
-        column3 = [12,4,9,14,15,3,10]
-        column4 = [16,17,18,5,14,11,19]
-        column5 = [20,19,18,21,17,9,23]
-        column6 = [16,8,24,25,20,26,27]
+        # symbols = {1:"a", 2:"n", 3:"R", 4:"D", 5:".", 6:"C", 7:"b", 8:"e", 9:"c", 10:"d", 11:"S", 12:"l", 13:"N", 14:"f", 15:"-", 16:"A", 17:"o", 18:"J", 19:"M", 20:"E", 21:",", 22:"F", 23:"g", 24:"m", 25:"T", 26:"L", 27:"/"}
+        # column1 = [1,2,3,13,5,6,7]
+        # column2 = [8,1,7,22,10,6,11]
+        # column3 = [12,4,9,14,15,3,10]
+        # column4 = [16,17,18,5,14,11,19]
+        # column5 = [20,19,18,21,17,9,23]
+        # column6 = [16,8,24,25,20,26,27]
 
-        column = choice([column1, column2, column3, column4, column5, column6])
-        #print(column1,column2,column3,column4,column5,column6)
-        #print(column)
-        if started == False:
+        # column = choice([column1, column2, column3, column4, column5, column6])
+        # #print(column1,column2,column3,column4,column5,column6)
+        # #print(column)
+        # if started == False:
             
-            self.keypad_correct = 0
+        #     self.keypad_correct = 0
             
-            self.keys = sample(column,4)
-            #print(keys)
-            self.symbol_1 = symbols[self.keys[0]]
-            self.symbol_2 = symbols[self.keys[1]]
-            self.symbol_3 = symbols[self.keys[2]]
-            self.symbol_4 = symbols[self.keys[3]]
+        #     self.keys = sample(column,4)
+        #     #print(keys)
+        #     self.symbol_1 = symbols[self.keys[0]]
+        #     self.symbol_2 = symbols[self.keys[1]]
+        #     self.symbol_3 = symbols[self.keys[2]]
+        #     self.symbol_4 = symbols[self.keys[3]]
 
-            self.label_1_color = "dim gray"
-            self.label_2_color = "dim gray"
-            self.label_3_color = "dim gray"
-            self.label_4_color = "dim gray"
+        #     self.label_1_color = "dim gray"
+        #     self.label_2_color = "dim gray"
+        #     self.label_3_color = "dim gray"
+        #     self.label_4_color = "dim gray"
 
-            self.key_order = []
-            for i in column:
-                for j in self.keys:
-                    if i == j:
-                        self.key_order.append(symbols[j])
-            #print(self.key_order)
-            #print(column)
-            self.Module_4_Started = True
+        #     self.key_order = []
+        #     for i in column:
+        #         for j in self.keys:
+        #             if i == j:
+        #                 self.key_order.append(symbols[j])
+        #     #print(self.key_order)
+        #     #print(column)
+        #     self.Module_4_Started = True
 
 
-        keypad_1 = Button(self, bg="lemon chiffon", text=self.symbol_1, font=("Wingdings", 25),
-                        borderwidth=10, command=lambda: keypad_check(self.symbol_1))
-        keypad_1.grid(row=3, column=0, sticky=N+S+E+W,
-                    padx=5, pady=5, columnspan=3)
+        # keypad_1 = Button(self, bg="lemon chiffon", text=self.symbol_1, font=("Wingdings", 25),
+        #                 borderwidth=10, command=lambda: keypad_check(self.symbol_1))
+        # keypad_1.grid(row=3, column=0, sticky=N+S+E+W,
+        #             padx=5, pady=5, columnspan=3)
         
-        keypad_2 = Button(self, bg="lemon chiffon", text=self.symbol_2, font=("Wingdings", 25),
-                        borderwidth=10, command=lambda: keypad_check(self.symbol_2))
-        keypad_2.grid(row=3, column=3, sticky=N+S+E+W,
-                    padx=5, pady=5, columnspan=3)
+        # keypad_2 = Button(self, bg="lemon chiffon", text=self.symbol_2, font=("Wingdings", 25),
+        #                 borderwidth=10, command=lambda: keypad_check(self.symbol_2))
+        # keypad_2.grid(row=3, column=3, sticky=N+S+E+W,
+        #             padx=5, pady=5, columnspan=3)
                     
-        keypad_3 = Button(self, bg="lemon chiffon", text=self.symbol_3, font=("Wingdings", 25),
-                        borderwidth=10, command=lambda: keypad_check(self.symbol_3))
-        keypad_3.grid(row=5, column=0, sticky=N+S+E+W,
-                    padx=5, pady=5, columnspan=3)
+        # keypad_3 = Button(self, bg="lemon chiffon", text=self.symbol_3, font=("Wingdings", 25),
+        #                 borderwidth=10, command=lambda: keypad_check(self.symbol_3))
+        # keypad_3.grid(row=5, column=0, sticky=N+S+E+W,
+        #             padx=5, pady=5, columnspan=3)
 
-        keypad_4 = Button(self, bg="lemon chiffon", text=self.symbol_4, font=("Wingdings", 25),
-                        borderwidth=10, command=lambda: keypad_check(self.symbol_4))
-        keypad_4.grid(row=5, column=3, sticky=N+S+E+W,
-                    padx=5, pady=5, columnspan=3)
+        # keypad_4 = Button(self, bg="lemon chiffon", text=self.symbol_4, font=("Wingdings", 25),
+        #                 borderwidth=10, command=lambda: keypad_check(self.symbol_4))
+        # keypad_4.grid(row=5, column=3, sticky=N+S+E+W,
+        #             padx=5, pady=5, columnspan=3)
 
-        label_1 = Label(self, text="", bg=self.label_1_color,
-                      borderwidth=10, relief="ridge")
-        label_1.grid(row=2, column=0, sticky=N+S+E+W, padx=5, pady=5, columnspan=3)
+        # label_1 = Label(self, text="", bg=self.label_1_color,
+        #               borderwidth=10, relief="ridge")
+        # label_1.grid(row=2, column=0, sticky=N+S+E+W, padx=5, pady=5, columnspan=3)
         
-        label_2 = Label(self, text="", bg=self.label_2_color,
-                      borderwidth=10, relief="ridge")
-        label_2.grid(row=2, column=3, sticky=N+S+E+W, padx=5, pady=5, columnspan=3)
+        # label_2 = Label(self, text="", bg=self.label_2_color,
+        #               borderwidth=10, relief="ridge")
+        # label_2.grid(row=2, column=3, sticky=N+S+E+W, padx=5, pady=5, columnspan=3)
 
-        label_3 = Label(self, text="", bg=self.label_3_color,
-                      borderwidth=10, relief="ridge")
-        label_3.grid(row=4, column=0, sticky=N+S+E+W, padx=5, pady=5, columnspan=3)
+        # label_3 = Label(self, text="", bg=self.label_3_color,
+        #               borderwidth=10, relief="ridge")
+        # label_3.grid(row=4, column=0, sticky=N+S+E+W, padx=5, pady=5, columnspan=3)
 
-        label_4 = Label(self, text="", bg=self.label_4_color,
-                      borderwidth=10, relief="ridge")
-        label_4.grid(row=4, column=3, sticky=N+S+E+W, padx=5, pady=5, columnspan=3)
+        # label_4 = Label(self, text="", bg=self.label_4_color,
+        #               borderwidth=10, relief="ridge")
+        # label_4.grid(row=4, column=3, sticky=N+S+E+W, padx=5, pady=5, columnspan=3)
 
-        for col in range(self.cols):
-            Grid.columnconfigure(self, col, weight=3)
+        # for col in range(self.cols):
+        #     Grid.columnconfigure(self, col, weight=3)
         
-        Grid.rowconfigure(self, 0, weight=0)
-        Grid.rowconfigure(self, 1, weight=0)
-        Grid.rowconfigure(self, 2, weight=5)
-        Grid.rowconfigure(self, 3, weight=10)
-        Grid.rowconfigure(self, 4, weight=5)
-        Grid.rowconfigure(self, 5, weight=10)
+        # Grid.rowconfigure(self, 0, weight=0)
+        # Grid.rowconfigure(self, 1, weight=0)
+        # Grid.rowconfigure(self, 2, weight=5)
+        # Grid.rowconfigure(self, 3, weight=10)
+        # Grid.rowconfigure(self, 4, weight=5)
+        # Grid.rowconfigure(self, 5, weight=10)
 
-        self.pack(fill=BOTH, expand=True)
+        # self.pack(fill=BOTH, expand=True)
 
-        if self.keypad_correct >= 4:
-                self.keypad_correct = 4
-                self.Module_4_Done = True
-                self.MainMenu()
+        # if self.keypad_correct >= 4:
+        #         self.keypad_correct = 4
+        #         self.Module_4_Done = True
+        #         self.MainMenu()
 
-        def keypad_check(input):
-            if self.keypad_correct <= 3:
-                if input == self.key_order[self.keypad_correct]:
-                    self.keypad_correct += 1
-                    if input == self.symbol_1:
-                        self.label_1_color = "lime green"
-                    elif input == self.symbol_2:
-                        self.label_2_color = "lime green"
-                    elif input == self.symbol_3:
-                        self.label_3_color = "lime green"
-                    elif input == self.symbol_4:
-                        self.label_4_color = "lime green"
-                    self.Module_Keypad(self.Module_4_Started)
-                else:
-                    self.keypad_correct = 0
+        # def keypad_check(input):
+        #     if self.keypad_correct <= 3:
+        #         if input == self.key_order[self.keypad_correct]:
+        #             self.keypad_correct += 1
+        #             if input == self.symbol_1:
+        #                 self.label_1_color = "lime green"
+        #             elif input == self.symbol_2:
+        #                 self.label_2_color = "lime green"
+        #             elif input == self.symbol_3:
+        #                 self.label_3_color = "lime green"
+        #             elif input == self.symbol_4:
+        #                 self.label_4_color = "lime green"
+        #             self.Module_Keypad(self.Module_4_Started)
+        #         else:
+        #             self.keypad_correct = 0
                     
-                    self.label_1_color = "dim gray"
-                    self.label_2_color = "dim gray"
-                    self.label_3_color = "dim gray"
-                    self.label_4_color = "dim gray"
-                    self.Module_Keypad(self.Module_4_Started)
-                    self.strike(1, 4, 2)
+        #             self.label_1_color = "dim gray"
+        #             self.label_2_color = "dim gray"
+        #             self.label_3_color = "dim gray"
+        #             self.label_4_color = "dim gray"
+        #             self.Module_Keypad(self.Module_4_Started)
+        #             self.strike(1, 4, 2)
 
-            
-
-        
-
-        
 
     def Game_Over(self):
 #       GPIO.cleanup()
@@ -642,5 +638,7 @@ window.title("Continue Speaking And Everyone Lives")
 window.geometry("800x400")
 # generate the GUI
 p = MainGUI(window)
+
+k = Keypad
 # display the GUI and wait for user interaction
 p.mainloop()
