d290564c97e65754522f85c16cf6beb6ea50d6ca
EthanDunning <ethandunning2002@gmail.com>
2021-04-28 20:31:56 -0500

    tudsr

--- Keypad.py
+++ Keypad.py
@@ -4,22 +4,27 @@ from tkinter import *
 
 class Module_Keypad(Game):
     def __init__(self,other):
-        super().__init__(self)
+        super().__init__()
         self.other = other
         self.other.clearFrame()   
 
-        self.rows = 6
-        self.cols = 6
-        self.loc = "Keypad"
+        self.other.rows = 6
+        self.other.cols = 6
+        self.other.loc = "Keypad"
 
-        self.pause_button(0, 0, 3)
-        self.back_button(0, 3, 3)
-        self.countdown(self.mins, self.secs, 1, 0, 2)
-        self.counter = self.after(1000, self.update_timer, 1, 0, 2)
-        self.location(1, 2, 2)
-        self.health(1, 4, 2)
+        self.Module_Started = False
+        self.Module_Done = False
 
-    def main(self):
+        self.other.pause_button(0, 0, 3)
+        self.other.back_button(0, 3, 3)
+        self.other.countdown(self.other.mins, self.other.secs, 1, 0, 2)
+        self.other.counter = self.other.after(1000, self.other.update_timer, 1, 0, 2)
+        self.other.location(1, 2, 2)
+        self.other.health(1, 4, 2)
+
+        main(self.Module_Started)
+
+    def main(self, started):
 
         symbols = {1:"a", 2:"n", 3:"R", 4:"D", 5:".", 6:"C", 7:"b", 8:"e", 9:"c", 10:"d", 11:"S", 12:"l", 13:"N", 14:"f", 15:"-", 16:"A", 17:"o", 18:"J", 19:"M", 20:"E", 21:",", 22:"F", 23:"g", 24:"m", 25:"T", 26:"L", 27:"/"}
         column1 = [1,2,3,13,5,6,7]
@@ -55,7 +60,7 @@ class Module_Keypad(Game):
                         self.key_order.append(symbols[j])
             #print(self.key_order)
             #print(column)
-            self.Module_4_Started = True
+            self.Module_Started = True
 
 
         keypad_1 = Button(self, bg="lemon chiffon", text=self.symbol_1, font=("Wingdings", 25),
@@ -108,7 +113,7 @@ class Module_Keypad(Game):
 
         if self.keypad_correct >= 4:
                 self.keypad_correct = 4
-                self.Module_4_Done = True
+                self.Module_Done = True
                 self.MainMenu()
 
         def keypad_check(input):
@@ -123,7 +128,7 @@ class Module_Keypad(Game):
                         self.label_3_color = "lime green"
                     elif input == self.symbol_4:
                         self.label_4_color = "lime green"
-                    self.Module_Keypad(self.Module_4_Started)
+                    self.Module_Keypad(self.Module_Started)
                 else:
                     self.keypad_correct = 0
                     
@@ -131,5 +136,5 @@ class Module_Keypad(Game):
                     self.label_2_color = "dim gray"
                     self.label_3_color = "dim gray"
                     self.label_4_color = "dim gray"
-                    self.Module_Keypad(self.Module_4_Started)
+                    self.Module_Keypad(self.Module_Started)
                     self.strike(1, 4, 2)
