b30ce10ef348b0b89bb5d58491443c36a3f97e3a
LiterallyANoodle <mam178@latech.edu>
2021-05-01 22:19:39 -0500

    finalized colors input for wires puzzle

--- wires.py
+++ wires.py
@@ -1,6 +1,7 @@
 from Game import Game;
 from tkinter import *;
 
+DEBUG = True;
 '''
 This is the module mimicking the wires game from KTANE.
 The game will pause and allow the player to input the order of colored wires on the board,
@@ -14,6 +15,7 @@ class Wires(Game):
 	def __init__(self, other):
 		super().__init__(None, None);
 
+		self._wires = [];
 		self.wires = [];
 		self.correct = 0;
 		# This may seem unintuitive, but I am saving the baseGUI as a property
@@ -27,10 +29,11 @@ class Wires(Game):
 		return self._wires;
 	@wires.setter
 	def wires(self, value):
-		if (value in ["orange", "yellow", "green", "blue", "purple"]):
-			self._wires = value;
-		else:
-			self._wires = "orange";
+		# if (value in ["Orange", "Yellow", "Green", "Blue", "Purple"]):
+		# 	self._wires = value;
+		# else:
+		# 	self._wires.append("orange");
+		self._wires = value;
 	@property
 	def correct(self):														# the index of correct wire to cut
 		return self._correct;
@@ -57,10 +60,42 @@ class Wires(Game):
 		self.other.cols = 3;
 
 		# list of current colors in order
-		colors = [];
+		colors = ["Please input wire color order."];
+
+		# internal functions 
+		# this function will append the color to the list, then update the display label
+		def appendColor (c):
+
+			# remove initial text
+			try:
+				if (colors[0] == "Please input wire color order." or colors[0] == "There must be 5 wires."):
+					colors.pop();
+			except:
+				print("Initial message already deleted.");
+
+			# delete or append the color 
+			if (c == "red" and len(colors) > 0):
+				colors.pop();
+			elif (c != "red" and len(colors) < 5):
+				colors.append(c);
+			colorsLabel.configure(text=str(colors));
+
+		# function maps the player input colors into the object properties;
+		def recordColors(c):
+
+			# check that there are 5 wires in the list 
+			if (len(c) == 5):		
+				self.wires = c;
+			else:
+				colors = ["There must be 5 wires."];
+				colorsLabel.configure(text=str(colors));
+			if (DEBUG):
+				print(c);
+				print(self.wires);
+			return;
 
 		# button that finishes the wire sequence
-		done = Button(self.other, bg="green", text="Done.", font=("TexGyreAdventor", 25), borderwidth=10, activebackground="blue", command=lambda: self.recordColors());
+		done = Button(self.other, bg="green", text="Done.", font=("TexGyreAdventor", 25), borderwidth=10, activebackground="blue", command=lambda: recordColors(colors));
 		done.grid(row=0, column=0, sticky=N+S+E+W, padx=5, pady=5, columnspan=2);
 
 		# button that goes to the main menu
@@ -72,22 +107,20 @@ class Wires(Game):
 		colorsLabel.grid(row=1, column=0, sticky=N+S+E+W, padx=5, pady=5, columnspan=3);
 
 		# list of tuples. each one contains the color name and function that adds it to the list (or removes in the case of the backspace)
-		colorButtons = [("orange", lambda: appendColor("Orange")), ("yellow", lambda: appendColor("Yellow")), ("green", lambda: appendColor("Green")), ("blue", lambda: appendColor("Blue")), ("purple", lambda: appendColor("Purple")), ("red", lambda: appendColor("red"))];
+		colorButtons = [("Orange", lambda: appendColor("Orange")), ("Yellow", lambda: appendColor("Yellow")), ("Green", lambda: appendColor("Green")), ("Blue", lambda: appendColor("Blue")), ("Purple", lambda: appendColor("Purple")), ("red", lambda: appendColor("red"))];
 
-		# this function will append the color to the list, then update the display label
-		def appendColor (c):
-			if (c == "red" and len(colors) > 0):
-				colors.pop();
-			elif (c != "red" and len(colors) < 5):
-				colors.append(c);
-			colorsLabel.configure(text=str(colors));
+
 
 		# create the buttons in order
 		count = 0;
 		for row in range(2, self.other.rows):
 			for col in range(self.other.cols):
 
-				key = Button(self.other, bg=colorButtons[count][0], text=colorButtons[count][0], font=("TexGyreAdventor", 25), relief="groove", borderwidth=10, activebackground="grey", command=colorButtons[count][1]);
+				# make the delete button
+				if (colorButtons[count][0] == "red"):
+					key = Button(self.other, bg=colorButtons[count][0], text="f", font=("Wingdings 3", 25), relief="groove", borderwidth=10, activebackground="grey", command=colorButtons[count][1]);					
+				else:
+					key = Button(self.other, bg=colorButtons[count][0], text=colorButtons[count][0], font=("TexGyreAdventor", 25), relief="groove", borderwidth=10, activebackground="grey", command=colorButtons[count][1]);
 				key.grid(row=row, column=col, sticky=N+S+E+W, padx=1, pady=1);
 
 				count += 1;
@@ -95,16 +128,14 @@ class Wires(Game):
 		# configure and pack the grid for display
 		for row in range(self.other.rows):
 			Grid.rowconfigure(self.other, row, weight=1);
-		Grid.columnconfigure(self.other, 0, weight=4);			# make the done button bigger than the back button
+		Grid.columnconfigure(self.other, 0, weight=1);			# make the done button bigger than the back button
 		for col in range(1, self.other.cols):
 			Grid.columnconfigure(self.other, col, weight=1);
 		self.other.pack(fill=BOTH, expand=True);
 
 		self.chooseCorrect();
 
-	# function allows the player to input the order of the colored wires on the breadboard 
-	def recordColors(self):
-		pass;
+
 
 	# function picks the correct wire to pull based on the order of the wires
 	def chooseCorrect(self):
