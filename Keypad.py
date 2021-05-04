7a3720a94eabcba68767970c845852d36a9e6319
Ethan <ethandunning2002@gmail.com>
2021-05-04 01:27:17 -0500

    added button and keypad

---
+++ Keypad.py
@@ -0,0 +1,139 @@
+from Game import Game
+from tkinter import *
+from random import *
+
+
+class Module_Keypad(Game):
+    def __init__(self, other):
+        super().__init__()
+        self.other = other
+        self.name = "Keypad"
+        self.other.loc = "Keypad"
+        self.Module_Started = False
+        self.Module_Done = False
+        self.main(self.Module_Started)
+
+    def main(self, started):
+
+        self.other.clearFrame()
+        self.other.rows = 6
+        self.other.cols = 6
+        self.other.pause_button(0, 0, 3)
+        self.other.back_button(0, 3, 3)
+        self.other.countdown(1, 0, 2)
+        self.other.location(1, 2, 2)
+        self.other.health(1, 4, 2)
+
+        symbols = {1: "a", 2: "n", 3: "R", 4: "D", 5: ".", 6: "C", 7: "b", 8: "e", 9: "c", 10: "d", 11: "S", 12: "l", 13: "N",
+                   14: "f", 15: "-", 16: "A", 17: "o", 18: "J", 19: "M", 20: "E", 21: ",", 22: "F", 23: "g", 24: "m", 25: "T", 26: "L", 27: "/"}
+        column1 = [1, 2, 3, 13, 5, 6, 7]
+        column2 = [8, 1, 7, 22, 10, 6, 11]
+        column3 = [12, 4, 9, 14, 15, 3, 10]
+        column4 = [16, 17, 18, 5, 14, 11, 19]
+        column5 = [20, 19, 18, 21, 17, 9, 23]
+        column6 = [16, 8, 24, 25, 20, 26, 27]
+
+        column = choice([column1, column2, column3, column4, column5, column6])
+        #print(column1,column2,column3,column4,column5,column6)
+        #print(column)
+        if started == False:
+
+            self.keypad_correct = 0
+            self.keys = sample(column, 4)
+            self.symbol_1 = symbols[self.keys[0]]
+            self.symbol_2 = symbols[self.keys[1]]
+            self.symbol_3 = symbols[self.keys[2]]
+            self.symbol_4 = symbols[self.keys[3]]
+            self.label_1_color = "dim gray"
+            self.label_2_color = "dim gray"
+            self.label_3_color = "dim gray"
+            self.label_4_color = "dim gray"
+
+            self.key_order = []
+            for i in column:
+                for j in self.keys:
+                    if i == j:
+                        self.key_order.append(symbols[j])
+            #print(self.key_order)
+            #print(column)
+            self.Module_Started = True
+
+        keypad_1 = Button(self.other, bg="lemon chiffon", text=self.symbol_1, font=("Wingdings", 45),
+                          borderwidth=10, command=lambda: keypad_check(self.symbol_1))
+        keypad_1.grid(row=3, column=0, sticky=N+S+E+W,
+                      padx=5, pady=5, columnspan=3)
+
+        keypad_2 = Button(self.other, bg="lemon chiffon", text=self.symbol_2, font=("Wingdings", 45),
+                          borderwidth=10, command=lambda: keypad_check(self.symbol_2))
+        keypad_2.grid(row=3, column=3, sticky=N+S+E+W,
+                      padx=5, pady=5, columnspan=3)
+
+        keypad_3 = Button(self.other, bg="lemon chiffon", text=self.symbol_3, font=("Wingdings", 45),
+                          borderwidth=10, command=lambda: keypad_check(self.symbol_3))
+        keypad_3.grid(row=5, column=0, sticky=N+S+E+W,
+                      padx=5, pady=5, columnspan=3)
+
+        keypad_4 = Button(self.other, bg="lemon chiffon", text=self.symbol_4, font=("Wingdings", 45),
+                          borderwidth=10, command=lambda: keypad_check(self.symbol_4))
+        keypad_4.grid(row=5, column=3, sticky=N+S+E+W,
+                      padx=5, pady=5, columnspan=3)
+
+        label_1 = Label(self.other, text="", bg=self.label_1_color,
+                        borderwidth=10, relief="ridge")
+        label_1.grid(row=2, column=0, sticky=N+S+E +
+                     W, padx=5, pady=5, columnspan=3)
+
+        label_2 = Label(self.other, text="", bg=self.label_2_color,
+                        borderwidth=10, relief="ridge")
+        label_2.grid(row=2, column=3, sticky=N+S+E +
+                     W, padx=5, pady=5, columnspan=3)
+
+        label_3 = Label(self.other, text="", bg=self.label_3_color,
+                        borderwidth=10, relief="ridge")
+        label_3.grid(row=4, column=0, sticky=N+S+E +
+                     W, padx=5, pady=5, columnspan=3)
+
+        label_4 = Label(self.other, text="", bg=self.label_4_color,
+                        borderwidth=10, relief="ridge")
+        label_4.grid(row=4, column=3, sticky=N+S+E +
+                     W, padx=5, pady=5, columnspan=3)
+
+        for col in range(self.other.cols):
+            Grid.columnconfigure(self.other, col, weight=3)
+
+        Grid.rowconfigure(self.other, 0, weight=2)
+        Grid.rowconfigure(self.other, 1, weight=1)
+        Grid.rowconfigure(self.other, 2, weight=1)
+        Grid.rowconfigure(self.other, 3, weight=2)
+        Grid.rowconfigure(self.other, 4, weight=1)
+        Grid.rowconfigure(self.other, 5, weight=2)
+
+        self.other.pack(fill=BOTH, expand=True)
+
+        def keypad_check(input):
+            if self.keypad_correct <= 3:
+                if input == self.key_order[self.keypad_correct]:
+                    self.keypad_correct += 1
+                    if input == self.symbol_1:
+                        self.label_1_color = "lime green"
+                    elif input == self.symbol_2:
+                        self.label_2_color = "lime green"
+                    elif input == self.symbol_3:
+                        self.label_3_color = "lime green"
+                    elif input == self.symbol_4:
+                        self.label_4_color = "lime green"
+                    self.main(self.Module_Started)
+                else:
+                    self.keypad_correct = 0
+
+                    self.label_1_color = "dim gray"
+                    self.label_2_color = "dim gray"
+                    self.label_3_color = "dim gray"
+                    self.label_4_color = "dim gray"
+                    self.main(self.Module_Started)
+                    self.other.strike()
+
+        if self.keypad_correct >= 4:
+            self.keypad_correct = 4
+            self.Module_Done = True
+            self.other.MainMenu()
--- The_Button.py
+++ The_Button.py
@@ -1,2 +1,188 @@
-from guiBase.py import *
+from Game import Game
 import RPi.GPIO as GPIO
+from tkinter import *
+from random import *
+from math import *
+from time import *
+GPIO.setwarnings(False)
+
+
+class Module_The_Button(Game):
+    def __init__(self, other, button_input):
+        super().__init__()
+        self.other = other
+        self.name = "The Button"
+        self.other.loc = "The Button"
+        self.Module_Started = False
+        self.Module_Done = False
+        self.button = button_input
+
+        self.start = None
+        self.end = None
+        self.button_time = None
+        self.button_pressed = None
+
+        self.button_checker = None
+        self.main(self.Module_Started)
+        # self.button_test()
+
+    def main(self, started):
+
+        self.other.clearFrame()
+        self.other.rows = 3
+        self.other.cols = 6
+        self.other.pause_button(0, 0, 3)
+        self.other.back_button(0, 3, 3)
+        self.other.countdown(1, 0, 2)
+        self.other.location(1, 2, 2)
+        self.other.health(1, 4, 2)
+        self.Modules_Completed = 0
+        for module in self.other.Modules_Done:
+            if module == True:
+                self.Modules_Completed += 1
+
+        # print(self.Modules_Completed)
+
+        if started == False:
+            self.Module_Started = True
+            button_colors = ["red", "blue", "yellow", "white", "gray"]
+            button_labels = ["Abort", "Detonate", "Hold", "Press", "Disarm"]
+            self.button_color = choice(button_colors)
+            self.button_label = choice(button_labels)
+
+            strip_colors = ["red", "blue", "yellow", "white"]
+            self.strip_color = choice(strip_colors)
+
+        the_button = Label(self.other, bg=self.button_color, text=self.button_label, font=(
+            "TexGyreAdventor", 45), borderwidth=10, relief="raised")
+        the_button.grid(row=2, column=0, sticky=N+S+E +
+                        W, padx=5, pady=5, columnspan=5)
+
+        self.strip = Label(self.other, text="", bg="black",
+                           borderwidth=10, relief="ridge")
+        self.strip.grid(row=2, column=(self.other.cols-1),
+                        sticky=N+S+E+W, padx=5, pady=5, columnspan=1)
+
+        for row in range(0, self.other.rows):
+            Grid.rowconfigure(self.other, row, weight=2)
+        Grid.rowconfigure(self.other, 2, weight=5)
+
+        for col in range(self.other.cols):
+            Grid.columnconfigure(self.other, col, weight=5)
+
+        self.other.pack(fill=BOTH, expand=True)
+
+        try:
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(26, GPIO.OUT)
+            GPIO.setup(self.button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            GPIO.remove_event_detect(self.button)
+            GPIO.add_event_detect(
+                self.button, GPIO.BOTH, callback=lambda *a: self.button_check(), bouncetime=100)
+        except:
+            pass
+
+    def button_check(self):
+        if self.Module_Started == True:
+            self.button_pressed = None
+            #GPIO.output(26, GPIO.input(self.button))
+            if GPIO.input(self.button) == 1 and self.end == None and self.start == None:
+                self.start = time()
+                # print("started")
+                self.strip.configure(bg=self.strip_color)
+            if (GPIO.input(self.button) == 0 and self.start != None and self.end == None):
+                self.strip.configure(bg="black")
+                self.end = time()
+                # print("ended")
+                self.button_time = (self.end-self.start)
+                # print(self.button_time)
+                self.button_pressed = True
+                if self.button_time >= 1:
+                    self.button_pressed = False
+
+                self.start = None
+                self.end = None
+
+                if self.button_color == "blue" and self.button_label == "Abort":
+                    if self.button_pressed == True:
+                        self.button_win()
+                    else:
+                        self.other.strike()
+
+                elif self.button_label == "Detonate":
+                    if len(self.button_color) > 4:
+                        if self.button_pressed == True:
+                            self.button_win()
+                        else:
+                            self.other.strike()
+
+                    elif len(self.button_color) <= 4:
+                        self.held_button()
+
+                elif self.button_label == "Disarm":
+                    if len(self.button_color) <= 4:
+                        if self.button_pressed == True:
+                            self.button_win()
+                        else:
+                            self.other.strike()
+
+                    elif len(self.button_color) > 4:
+                        self.held_button()
+
+                elif self.button_color == "gray":
+                    self.held_button()
+
+                elif self.button_color == "yellow" and self.Modules_Completed <= 3:
+                    if self.button_pressed == True:
+                        self.button_win()
+                    else:
+                        self.other.strike()
+
+                elif self.button_color == "red":
+                    if self.button_pressed == True:
+                        self.button_win()
+                    else:
+                        self.other.strike()
+
+                if self.Modules_Completed == 5:
+                    if self.button_pressed == True:
+                        self.button_win()
+                    else:
+                        self.other.strike()
+
+                else:
+                    self.held_button()
+
+    def held_button(self):
+
+        # print(self.strip_color)
+        time = self.other.time.get()
+        # print(time)
+        if self.strip_color == "red":
+            if "1" in time:
+                print("win")
+                self.button_win()
+            else:
+                self.other.strike()
+
+        elif self.strip_color == "yellow":
+            if "3" in time:
+                self.button_win()
+            else:
+                self.other.strike()
+
+        elif self.strip_color == "blue":
+            if "7" in time:
+                self.button_win()
+            else:
+                self.other.strike()
+
+        elif self.strip_color == "white":
+            if "9" in time:
+                self.button_win()
+            else:
+                self.other.strike()
+
+    def button_win(self):
+        self.Module_Done = True
+        self.other.MainMenu()
